7468a9f36003978f2ffa677446cff8071d14c2d8
Zhijun Sheng <zhijunsheng@gmail.com>
2018-02-27 07:28:50 -0500

    created MortiseTenonPuzzle cpp project in xcode

---
+++ MortiseTenonPuzzle/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MortiseTenonPuzzle
+//
+//  Created by Donald Sheng on 2018-02-27.
+//  Copyright © 2018 GoldThumb Inc. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
