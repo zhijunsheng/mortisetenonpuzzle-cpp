7aef38bafc7a26e770b7837b7d6d8444cbcccf7c
Zhijun Sheng <zhijunsheng@gmail.com>
2018-02-28 02:45:36 -0500

    added testVariableSize

--- MortiseTenonPuzzleTests/RecursionTests.mm
+++ MortiseTenonPuzzleTests/RecursionTests.mm
@@ -16,6 +16,29 @@ using namespace std;
 
 @implementation RecursionTests
 
+- (void)testVariableSize {
+    bool b;
+    char c;
+    int n;
+    long l;
+    long long ll;
+    float f;
+    double d;
+    long double ld;
+    
+    XCTAssertEqual(1, sizeof b);
+    XCTAssertEqual(1, sizeof c);
+    XCTAssertEqual(4, sizeof n);
+    XCTAssertEqual(8, sizeof l);
+    XCTAssertEqual(8, sizeof ll);
+    XCTAssertEqual(4, sizeof f);
+    XCTAssertEqual(8, sizeof d);
+    XCTAssertEqual(16, sizeof ld);
+    
+    XCTAssertEqual(1, (char*)&b - (char*)&c);
+    XCTAssertEqual(8, (char*)&l - (char*)&ll);
+}
+
 - (void)testFactorial {
     XCTAssertEqual(120, RecursionTestsUtils::factorial(5));
 }
