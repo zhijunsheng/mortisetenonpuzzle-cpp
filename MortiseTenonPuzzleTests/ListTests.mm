d2052c989b3e56a61ce5af2fd08ed28441b45877
Zhijun Sheng <zhijunsheng@gmail.com>
2018-02-28 11:34:46 -0500

    added freelist support to Link

--- MortiseTenonPuzzleTests/ListTests.mm
+++ MortiseTenonPuzzleTests/ListTests.mm
@@ -190,8 +190,10 @@ public:
     }
 };
 
-// Singly linked list node
+// Singly linked list node with freelist support
 template <typename E> class Link {
+private:
+    static Link<E>* freelist;   // Reference to freelist head
 public:
     E element;      // Value for this node
     Link *next;     // Pointer to next node in list
@@ -206,7 +208,23 @@ public:
     Link(Link* nextval = NULL) {
         next = nextval;
     }
+    
+    void* operator new(size_t) {                    // Overloaded new operator
+        if (freelist == NULL) return ::new Link;    // Create space
+        Link<E>* temp = freelist;                   // Can take from freelist
+        freelist = freelist->next;
+        return temp;                                // Return the link
+    }
+    
+    void operator delete(void* ptr) {               // Overloaded delete operator
+        ((Link<E>*)ptr)->next = freelist;           // Put on freelist
+        freelist = (Link<E>*)ptr;
+    }
 };
+
+// The freelist head pointer is actually created here
+template <typename E>
+Link<E>* Link<E>::freelist = NULL;
 
 // Linked list implementation
 template <typename E> class LList: public List<E> {
