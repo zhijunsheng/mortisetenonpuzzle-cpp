bce1907a14c2b2048c7fce8c77c20663fea1336f
Zhijun Sheng <zhijunsheng@gmail.com>
2018-02-28 01:40:07 -0500

    created SimpleLoopTests.mm

--- MortiseTenonPuzzle/main.cpp
+++ MortiseTenonPuzzle/main.cpp
@@ -6,10 +6,22 @@
 //  Copyright © 2018 GoldThumb Inc. All rights reserved.
 //
 
+#include <cstdio>
+#include <cstdlib>
 #include <iostream>
+using namespace std;
+
+int add(int x, int y) {
+    return x + y;
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    int sum = add(3, 9);
+    
+    cout << sum << endl;
+    
     return 0;
 }
+
+
---
+++ MortiseTenonPuzzleTests/SimpleLoopTests.mm
@@ -0,0 +1,71 @@
+//
+//  SimpleLoopTests.mm
+//  MortiseTenonPuzzleTests
+//
+//  Created by Donald Sheng on 2018-02-28.
+//  Copyright © 2018 GoldThumb Inc. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import <iostream>
+using namespace std;
+
+@interface SimpleLoopTests : XCTestCase
+
+@end
+
+@implementation SimpleLoopTests
+
+- (void)testAdd {
+    XCTAssertEqual(8, Utils::add(3, 5));
+    
+    cout << "hello .mm" << endl;
+}
+
+- (void)testFindMax {
+    int arr[5] = {3, 7, 2, 13, 5};
+    XCTAssertEqual(13, Utils::findMaxUsingForLoop(arr, 5));
+    XCTAssertEqual(13, Utils::findMaxUsingWhileLoop(arr, 5));
+}
+
+- (void)testFactorial {
+    XCTAssertEqual(120, Utils::factorial(5));
+}
+
+class Utils {
+public:
+    static int factorial(int n) {
+        int total = 1;
+        for (int i = 1; i <= n; i++) {
+            total *= i;
+        }
+        return total;
+    }
+    
+    static int add(int x, int y) {
+        return x + y;
+    }
+    
+    static int findMaxUsingWhileLoop(int ints[], int numInts) {
+        int i = 1, max = ints[0] ;
+        while (i < numInts) {
+            if (max < ints[i]) {
+                max = ints[i];
+            }
+            i++;
+        }
+        return max;
+    }
+    
+    static int findMaxUsingForLoop(int ints[], int numInts) {
+        int max = ints[0];
+        for(int i = 1; i < numInts; i++) {
+            if(max < ints[i]) {
+                max = ints[i];
+            }
+        }
+        return max;
+    }
+};
+
+@end
